d5f444f3b7a64132e53077efebfa01b0a5496862
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-09-29 21:58:01 +0000

    Automatically backed up by Learn

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -30,7 +30,7 @@ def fizzbuzz (int)
     return "Fizz"
   elsif int % 5 == 0
     return "Buzz"
-  if ((int % 3 == 0) && (int % 5 == 0))
+  elsif ((int % 3 == 0) && (int % 5 == 0))
     return "FizzBuzz"
   end
 end
