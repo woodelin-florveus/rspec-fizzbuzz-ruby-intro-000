c6e3658096b8c07a2cba78d7c0f3e1f0a2ce246e
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-09-29 21:54:48 +0000

    Automatically backed up by Learn

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -25,11 +25,11 @@ describe "fizzbuzz" do
   end
 end
 
-def fizzbuzz (num)
-  if num % 3 == 0
-    return "Fizz"
-  elsif num % 5 == 0
-    return "Buzz"
-  elsif (num % 3 == 0) && (num % 5 == 0)
-    return "FizzBuzz"
-  end
+# def fizzbuzz (num)
+#   if num % 3 == 0
+#     return "Fizz"
+#   elsif num % 5 == 0
+#     return "Buzz"
+#   elsif (num % 3 == 0) && (num % 5 == 0)
+#     return "FizzBuzz"
+#   end
